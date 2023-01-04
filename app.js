b57086fcc7402b70d36d65c723b3ab35ea36b6b0
Pahaut Quentin <quentinpahaut@gmail.com>
2023-01-04 22:23:52 +0100

    ADD: middleware on app.js AND error and logging management on server.js

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next)=>{
+    console.log('requête reçue');
+    next();
+})
 
+app.use((req, res, next)=>{
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next)=>{
+    res.json({message:"Vla la res ! YoloOO !"})
+    next();
+})
+
+app.use((req,res,)=>{
+    console.log('Réponse envoyée avec succès !')
+})
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,49 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la nouvelle réponse du serveur !');
-});
+/* renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne */
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
-server.listen(process.env.PORT || 3000);
+    if(isNaN(port)){
+        return val;
+    }
+    if (port >= 0){
+        return port;
+    }
+    return false;
+}
+//lance le serveur express et normalise le port
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES' :
+            console.error(bind + 'require elevated privileges. ');
+            process.exit(1);
+            breack;
+        case 'EADDRINUSE' : 
+            process.exit(1);
+            breack;
+        default:
+            throw error;
+    }
+};
+
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+})
+
+server.listen(port);
