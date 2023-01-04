c81223b99a058d2602475482b673262058a3ee28
Pahaut Quentin <quentinpahaut@gmail.com>
2023-01-04 19:03:59 +0100

    ADD : app.js for express

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la nouvelle réponse du serveur !');
+});
+
+server.listen(process.env.PORT || 3000);
