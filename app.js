4f194fb5d37754f7c86fcfcfafff8209a2e74ab0
Pahaut Quentin <quentinpahaut@gmail.com>
2023-01-09 00:24:58 +0100

    FIX: Problems with route in app.js

--- app.js
+++ app.js
@@ -60,10 +60,20 @@ app.post('/api/stuff', (req,res,next) =>{
     .catch((error => res.status(400).json({ error })));
 })
 
-app.use('/api/stuff', (req, res, next) => {
+// interprète les requêtes GET reçues du front-end munis d'un "id" 
+// l'id est fourni par le front et défini un nouveau chemin dynamique vers l'objet désiré
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }))
-  });
+});
+
+
 
 module.exports = app;
